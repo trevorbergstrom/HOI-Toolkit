c3f963bfc849762ef46ce972dfce3cdd4f5738e0
Trevor <trevor.bergstrom@gmail.com>
2020-04-08 23:17:27 -0400

    Working Forward Pass

--- Dataset/Tools/dataset_load.py
+++ Dataset/Tools/dataset_load.py
@@ -4,20 +4,12 @@ from torch.utils.data import Dataset
 import torch
 from torchvision.transforms import ToTensor, ToPILImage
 from PIL import Image
-
-# anno_bb.mat file in dict:
-bbs = loadmat('../anno_bbox.mat')
-print(bbs.keys())
-
-
-# Training Bounding Boxes:
-bb_train = bbs['bbox_train']
-# Testing Bounding Boexs:
-bb_test = bbs['bbox_test']
-# Actions:
-actions = bbs['list_action']
+import os
 
 '''
+This is the HICO DET dataloader class
+It requires paths to the image folders and the path to the annotations file!
+
 How the structures look:
 Img_list:
     [ [img1_name, w, h, d, [im1_hoi_list] ],
@@ -40,119 +32,133 @@ Conn_list:
 
 '''
 
-def convert_bb(data_split, data_list):
-
-    img_list = []
-    itr = 0
-
-    # For each image in the split:
-    for i in data_list[0]:
-
-        # Get the image name
-        img_name = i[0][0]
-
-        # Get the image properties:
-        width = i[1][0][0][0][0][0]
-        height = i[1][0][0][1][0][0]
-        depth = i[1][0][0][2][0][0]
-
-        # Append to list of props for current image:
-        img_line = []
-        img_line.append(img_name)
-        img_line.append(width)
-        img_line.append(height)
-        img_line.append(depth)
-
-        # Add image properties to the image list:
-        img_list.append(img_line)
-
-        # Get the number of HOIs :
-        num_hoi = i[2].size
-
-        hoi_list = []
-
-        # For each hoi:
-        for l in range(num_hoi):
-
-            # Get critical properties of the HOI:
-            invisible = i[2][0][l][4][0][0]
-            hoi_id = i[2][0][l][0][0][0]
-
-            # Create a line container to hold properties of the HOI:
-            hoi_inst = []
-            hoi_inst.append(hoi_id)
-            hoi_inst.append(invisible)
-
-            # Create containers for each:
-            bb_h_list = []
-            bb_o_list = []
-            conn_list = []
-
-            # If a HOI is invisible, no HOIs properties will be listed here:
-            if not invisible:
-
-                # For all human bounding boxes:
-                for m in range(int(i[2][0][l][1].size)):
-                    bb_h_line = []
-
-                    # human bbox coords:
-                    x1_h = i[2][0][l][1][0][m][0][0][0]
-                    x2_h = i[2][0][l][1][0][m][1][0][0]
-                    y1_h = i[2][0][l][1][0][m][2][0][0]
-                    y2_h = i[2][0][l][1][0][m][3][0][0]
-
-                    bb_h_line.append(x1_h)
-                    bb_h_line.append(x2_h)
-                    bb_h_line.append(y1_h)
-                    bb_h_line.append(y2_h)
-
-                    bb_h_list.append(bb_h_line)
-
-                # For all object bboxes
-                for n in range(i[2][0][l][2].size):
-                    bb_o_line = []
-
-                    # object bbox coords:
-                    x1_o = i[2][0][l][2][0][n][0][0][0]
-                    x2_o = i[2][0][l][2][0][n][1][0][0]
-                    y1_o = i[2][0][l][2][0][n][2][0][0]
-                    y2_o = i[2][0][l][2][0][n][3][0][0]
-
-                    bb_o_line.append(x1_o)
-                    bb_o_line.append(x2_o)
-                    bb_o_line.append(y1_o)
-                    bb_o_line.append(y2_o)
-
-                    bb_o_list.append(bb_o_line)
-
-                # For all connections
-                for p in range(int(i[2][0][l][3].size / 2)): #screwy indexing here for some reason
-                    conn_line = []
-                    connection = i[2][0][l][3][p]
-                    conn_line.append(connection[0])
-                    conn_line.append(connection[1])
-
-                    conn_list.append(conn_line)
-
-            # Lists for hoi_instance
-            hoi_inst.append(bb_h_list)
-            hoi_inst.append(bb_o_list)
-            hoi_inst.append(conn_list)
-
-            # Adding entry to HOI file for image
-            hoi_list.append(hoi_inst)
-
-        # Writing Image Index file:
-        img_line.append(hoi_list)
-
-    img_list.append(img_line)
-    return img_list
-
 class HICO_DET_Dataloader(Dataset):
 
-    def __init__(self, train_csv_path, test_csv_path, train_data_path, test_data_path):
-        self.img_train = convert_bb('train', bb_train)
-        self.img_test = convert_bb('test', bb_test)
+    def convert_bb(self, data_split, data_list):
+
+        img_list = []
+        itr = 0
+
+        # For each image in the split:
+        for i in data_list[0]:
+
+            # Get the image name
+            img_name = i[0][0]
+
+            # Get the image properties:
+            width = i[1][0][0][0][0][0]
+            height = i[1][0][0][1][0][0]
+            depth = i[1][0][0][2][0][0]
+
+            # Append to list of props for current image:
+            img_line = []
+            img_line.append(img_name)
+            img_line.append(width)
+            img_line.append(height)
+            img_line.append(depth)
+
+            # Add image properties to the image list:
+            img_list.append(img_line)
+
+            # Get the number of HOIs :
+            num_hoi = i[2].size
+
+            hoi_list = []
+
+            # For each hoi:
+            for l in range(num_hoi):
+
+                # Get critical properties of the HOI:
+                invisible = i[2][0][l][4][0][0]
+                hoi_id = i[2][0][l][0][0][0]
+
+                # Create a line container to hold properties of the HOI:
+                hoi_inst = []
+                hoi_inst.append(hoi_id)
+                hoi_inst.append(invisible)
+
+                # Create containers for each:
+                bb_h_list = []
+                bb_o_list = []
+                conn_list = []
+
+                # If a HOI is invisible, no HOIs properties will be listed here:
+                if not invisible:
+
+                    # For all human bounding boxes:
+                    for m in range(int(i[2][0][l][1].size)):
+                        bb_h_line = []
+
+                        # human bbox coords:
+                        x1_h = i[2][0][l][1][0][m][0][0][0]
+                        x2_h = i[2][0][l][1][0][m][1][0][0]
+                        y1_h = i[2][0][l][1][0][m][2][0][0]
+                        y2_h = i[2][0][l][1][0][m][3][0][0]
+
+                        bb_h_line.append(x1_h)
+                        bb_h_line.append(x2_h)
+                        bb_h_line.append(y1_h)
+                        bb_h_line.append(y2_h)
+
+                        bb_h_list.append(bb_h_line)
+
+                    # For all object bboxes
+                    for n in range(i[2][0][l][2].size):
+                        bb_o_line = []
+
+                        # object bbox coords:
+                        x1_o = i[2][0][l][2][0][n][0][0][0]
+                        x2_o = i[2][0][l][2][0][n][1][0][0]
+                        y1_o = i[2][0][l][2][0][n][2][0][0]
+                        y2_o = i[2][0][l][2][0][n][3][0][0]
+
+                        bb_o_line.append(x1_o)
+                        bb_o_line.append(x2_o)
+                        bb_o_line.append(y1_o)
+                        bb_o_line.append(y2_o)
+
+                        bb_o_list.append(bb_o_line)
+
+                    # For all connections
+                    for p in range(int(i[2][0][l][3].size / 2)): #screwy indexing here for some reason
+                        conn_line = []
+                        connection = i[2][0][l][3][p]
+                        conn_line.append(connection[0])
+                        conn_line.append(connection[1])
+
+                        conn_list.append(conn_line)
+
+                # Lists for hoi_instance
+                hoi_inst.append(bb_h_list)
+                hoi_inst.append(bb_o_list)
+                hoi_inst.append(conn_list)
+
+                # Adding entry to HOI file for image
+                hoi_list.append(hoi_inst)
+
+            # Writing Image Index file:
+            img_line.append(hoi_list)
+
+        img_list.append(img_line)
+        return img_list
+
+    def __load_mat__(self, matfile):
+
+        # anno_bb.mat file in dict:
+        bbs = loadmat(matfile)
+        return bbs
+
+    def __init__(self, train_data_path, test_data_path, matfile):
+        bbs = self.__load_mat__(matfile)
+        # Training Bounding Boxes:
+        bb_train = bbs['bbox_train']
+        # Testing Bounding Boexs:
+        bb_test = bbs['bbox_test']
+        # Actions:
+        actions = bbs['list_action']
+
+        self.img_train = self.convert_bb('train', bb_train)
+        self.img_test = self.convert_bb('test', bb_test)
         self.test_data_path = test_data_path
         self.train_data_path = test_data_path
 
@@ -173,11 +179,11 @@ class HICO_DET_Dataloader(Dataset):
         img_anno = 0
 
         if split == 'test':
-            img = get_image(idx, split) # Here open the image and then load it into a torchtensor??
             img_anno = self.img_test[idx]
+            img = self.get_image(img_anno[0], split) # Here open the image and then load it into a torchtensor??
         else:
-            img = get_image(idx, split) # here open again
             img_anno = self.img_train[idx]
+            img = self.get_image(img_anno[0], split) # here open again
 
         if self.transform is not None:
             img = self.transform(img)
@@ -187,24 +193,45 @@ class HICO_DET_Dataloader(Dataset):
 
         return sample
 
-    def __get_human_crop__(self, idx, split):
-
-        img = 0
-        img_anno = 0
+    def __get_object_crop__(self, idx, split):
 
         if split == 'test':
-            img = get_image(idx, split) # Here open the image and then load it into a torchtensor??
             img_anno = self.img_test[idx]
+            img = self.get_image(img_anno[0], split) # Here open the image and then load it into a torchtensor??
         else:
-            img = get_image(idx, split) # here open again
             img_anno = self.img_train[idx]
+            img = self.get_image(img_anno[0], split) # here open again
 
-        if img_anno == 0:
-            bbox_h = img_anno[idx][2][0]
+        if img_anno[4][0][1] == 0:
+            bbox = img_anno[4][0][3][0]
+            obj = img.crop(( bbox[0], bbox[2], bbox[1], bbox[3] ))
+            return obj
+        else:
+            return 0
+
+    def __get_human_crop__(self, idx, split):
+
+        if split == 'test':
+            img_anno = self.img_test[idx]
+            img = self.get_image(img_anno[0], split) # Here open the image and then load it into a torchtensor??
+        else:
+            img_anno = self.img_train[idx]
+            img = self.get_image(img_anno[0], split) # here open again
+
+        if img_anno[4][0][1] == 0:
+            bbox_h = img_anno[4][0][2][0]
             human = img.crop(( bbox_h[0], bbox_h[2], bbox_h[1], bbox_h[3] ))
             return human
         else:
             return 0
 
-data = HICO_DET_Dataloader()
+'''
+Testing stuff delete later
+data = HICO_DET_Dataloader('~/Documents/hico/images/train2015', 'test2015','../anno_bbox.mat')
+
+data.__get_human_crop__(1, 'test').show()
+data.__get_object_crop__(1, 'test').show()
+
+NEED TO FIX: image paths...
+'''
 
--- HO_RCNN/Streams/human_stream.py
+++ HO_RCNN/Streams/human_stream.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 from PIL import Image
 import numpy as np
+import sys
+
+sys.path.append('../../Dataset/Tools')
+from dataset_load import HICO_DET_Dataloader
 
 class Human_net(nn.Module):
 
@@ -33,39 +37,46 @@ class Human_net(nn.Module):
                 )
         self.linear_layers = nn.Sequential(
                 # FC-Layer 1:
-                nn.Linear(256, 4096),
+                nn.Linear(9216, 1),
                 nn.ReLU(inplace=True),
                 nn.Dropout(inplace=True),
                 # FC-Layer 2:
-                nn.Linear(4096, 4096),
+                nn.Linear(1, 4096),
                 nn.ReLU(inplace=True),
                 nn.Dropout(inplace=True),
                 # Final Class Score layer:
                 nn.Linear(4096, 600)
-        )
+                )
+        '''
+        self.cnn_layers = nn.Sequential(
+                nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1)#,
+                #nn.ReLU(inplace=True),
+                #nn.MaxPool2d(kernel_size=3)
+                )
+        '''
+
     def forward(self, x):
         x = self.cnn_layers(x)
+        print(x.shape)
         x = x.view(x.size(0), -1)
+        print('FLATTEN RES:')
+        print(x.shape)
         x = self.linear_layers(x)
         return x
 
 model = Human_net()
-
-img = Image.open('plane.jpg')
+model.cuda()
+data = HICO_DET_Dataloader('/Documents/hico/images/train2015', '../../Dataset/Tools/test2015', '../../Dataset/anno_bbox.mat')
+img = data.__get_human_crop__(1, 'test')
 img = img.resize((256,256))
 #img = img.convert('RGB')
 img = np.asarray(img).transpose(-1,0,1)
-img = torch.from_numpy(np.asarray(img))
-
-
-print(img.shape)
-imgs = []
-imgs.append(img)
-
-r_img = torch.randn(1, 3, 256, 256)
-print(r_img.shape)
+img = torch.from_numpy(img)
+img = img.unsqueeze(0).cuda()
+#img2 = torch.randn(3,1,11,11)
 
 with torch.no_grad():
-    res = model(r_img)
+    res = model(img.float())
 
 print(res.shape)
+print(res)
